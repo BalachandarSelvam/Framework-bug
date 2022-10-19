a10dcf9f4ac6bf8afdb7214f97660e1be35b25c2
BalachandarSelvam <balachandar.selvam@syncfusion.com>
2022-10-19 22:09:47 +0530

    ScrollView

---
+++ ScrollView-Bug/ScrollViewBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ScrollViewBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ScrollView-Bug/ScrollViewBug/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ScrollViewBug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
