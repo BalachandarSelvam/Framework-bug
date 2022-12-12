9d58cd6acd706a2257c2a9d221a4729017698464
BalachandarSelvam <balachandar.selvam@syncfusion.com>
2022-12-12 12:46:42 +0530

    Content clip

---
+++ ContentClip-Bug/ContentClipBug/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace ContentClipBug;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var heightPercentage = (this.Height - 100) / this.Height;
+        AbsoluteLayout.SetLayoutBounds(this.contentView, new Rect(0, 0, 1, heightPercentage));
+
+        this.contentView.Content.Clip = new RoundRectangleGeometry(new CornerRadius(20,20,0,0), new Rect(0, 0, this.Width, this.Height - 100));
+    }
+}
+
---
+++ ContentClip-Bug/ContentClipBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ContentClipBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
